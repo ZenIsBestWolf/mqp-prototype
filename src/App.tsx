744df994c7050d362f440b399584b23bfd41f485
Zen Dignan <zen@dignan.dev>
2025-10-08 13:14:28 -0400

    feat: Add stat charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import { Card, CardBody, CardTitle, Col, Container, Row } from 'reactstrap';
 import { NavBar } from './components/NavBar';
 import { ServiceStatusCard } from './components/ServiceStatusCard';
-import { ApplicationContext, ApplicationData, DEFAULT_APP_VALUES, ServiceStatus } from './utils';
+import { ApplicationContext, ApplicationData, DEFAULT_APP_VALUES, useApp } from './utils';
+import { Line } from 'react-chartjs-2';
+import {
+  ArcElement,
+  BarController,
+  BarElement,
+  CategoryScale,
+  Chart,
+  Legend,
+  LinearScale,
+  LineController,
+  LineElement,
+  PieController,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
 
 export const App: FC = () => {
   const [application, setApplication] = useState<ApplicationData>(DEFAULT_APP_VALUES);
@@ -32,17 +48,15 @@ export const App: FC = () => {
             <Col xl="10" md="12">
               {/* General Status Indicator */}
               <h3>Current Status</h3>
-              <ServiceStatusMessage
-                status={application.globalServiceStatus}
-                messageOverride={application.serviceMessage}
-              />
+              <ServiceStatusMessage />
               {/* Service matrix */}
               <br />
               <h3>Individual Services</h3>
-              <ServiceMatrix serviceMap={application.serviceMap} />
+              <ServiceMatrix />
               {/* Statistical graphs */}
               <br />
               <h3>Statistics</h3>
+              <StatisticGraph />
             </Col>
             {/* Spacer */}
             <Col xl="1" />
@@ -53,10 +67,9 @@ export const App: FC = () => {
   );
 };
 
-const ServiceStatusMessage: FC<{
-  readonly status: ServiceStatus;
-  readonly messageOverride?: string;
-}> = ({ status, messageOverride }) => {
+const ServiceStatusMessage: FC = () => {
+  const { globalServiceStatus: status, serviceMessage: messageOverride } = useApp();
+
   let color: string;
   let message: string;
   switch (status) {
@@ -90,9 +103,8 @@ const ServiceStatusMessage: FC<{
   );
 };
 
-const ServiceMatrix: FC<{ readonly serviceMap: Record<string, ServiceStatus> }> = ({
-  serviceMap,
-}) => {
+const ServiceMatrix: FC = () => {
+  const { serviceMap } = useApp();
   const serviceList = Object.keys(serviceMap);
 
   return (
@@ -105,5 +117,78 @@ const ServiceMatrix: FC<{ readonly serviceMap: Record<string, ServiceStatus> }> 
         ))}
       </Row>
     </Container>
+  );
+};
+
+Chart.register(
+  LineController,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  PieController,
+  ArcElement,
+  Title,
+  Legend,
+  Tooltip,
+  BarController,
+  BarElement,
+);
+
+interface MetricData {
+  timestamp: number;
+  value: number;
+}
+
+const StatisticGraph: FC = () => {
+  const metrics: MetricData[] = useMemo(() => {
+    const result: MetricData[] = [];
+
+    for (let i = 0; i < 100; i++) {
+      // Create an entry for once every 10 seconds
+      result.push({
+        timestamp: Date.now() - 10000 * i,
+        value: Math.floor(Math.random() * 5) + 90,
+      } satisfies MetricData);
+    }
+
+    return result.sort((a, b) => a.timestamp - b.timestamp);
+  }, []);
+
+  console.log(metrics);
+
+  return (
+    <Line
+      data={{
+        labels: metrics.map((m) => new Date(m.timestamp).toLocaleTimeString()),
+        datasets: [
+          {
+            label: 'API Latency',
+            backgroundColor: `rgb(255, 255, 255)`,
+            borderColor: `rgb(255, 255, 255)`,
+            data: metrics.map((m) => m.value),
+            // fill: true,
+          },
+        ],
+      }}
+      options={{
+        plugins: {
+          legend: { display: false },
+          // title: { display: true, text: 'API Uptime - Response in Milliseconds' },
+          // tooltip: { mode: 'index', intersect: false },
+        },
+        responsive: true,
+        scales: {
+          y: {
+            min: 80,
+            max: Math.max(...metrics.map((m) => m.value)) + 30,
+            title: { display: true, text: 'Response Time (ms)' },
+          },
+          x: {
+            title: { display: true, text: 'Timestamp' },
+          },
+        },
+      }}
+    />
   );
 };
